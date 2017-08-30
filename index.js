92cfe4b2c322dd7ac7581c5a2b19982e7e626553
Dennis Dang <dangggdennis@gmail.com>
2017-08-30 16:15:35 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
+
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send({ hi: "there" });
-});
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, function() {
+app.listen(PORT, () => {
 	console.log("server listening on port 5000");
 });
