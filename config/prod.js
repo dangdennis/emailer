83d617d70373c0f2e425ef1160efc892fb633de3
Dennis Dang <dangggdennis@gmail.com>
2017-09-09 14:25:41 -0700

    complete auth flow

--- config/prod.js
+++ config/prod.js
@@ -6,14 +6,3 @@ module.exports = {
 	mongoURI: process.env.MONGO_URI,
 	cookieKey: process.env.COOKIE_KEY
 };
-
-// module.exports = {
-// 	googleClientID:
-// 		"343620775256-c82a3o4qcb4msho3mnkdjref83fvlreu.apps.googleusercontent.com",
-// 	googleClientSecret: "6pTh0zyMVNycHC0lxxJBbic_",
-// 	facebookAppID: "194943744379151",
-// 	facebookAppSecret: "dfe91a2872ab1f0288b0f3b5e8bd6925",
-// 	mongoURI:
-// 		"mongodb://dennisdangprod:dennisdangprod@ds129024.mlab.com:29024/emaily-dang-prod",
-// 	cookieKey: ""
-// };
